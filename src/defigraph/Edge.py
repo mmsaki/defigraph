17fbc5ba20bbff15c7b11f7779c8f60efa105dbf
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2024-10-15 18:09:27 -0500

    😵‍💫 fix  pool address

--- src/defigraph/Edge.py
+++ src/defigraph/Edge.py
@@ -2,12 +2,11 @@ from defigraph.Vertex import Vertex
 from defigraph.Pool import Pool
 
 class Edge:
-  def __init__(self, u: Vertex, v: Vertex, weight: float=None):
+  def __init__(self, u: Vertex, v: Vertex, pool_address: str, weight: float=None):
     self.u = u
     self.v = v
     self.weight = weight
-    address = ""
-    self.pool = Pool(address=address,token0=u, token1=v)
+    self.pool = Pool(address=pool_address,token0=u, token1=v)
 
   def __repr__(self):
     if not self.weight:
