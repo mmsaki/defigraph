013e261868a6fe46b32ec4f197cebe2b2276e17a
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2024-10-16 11:51:06 -0500

    🪺 make edge iterable

--- src/defigraph/Edge.py
+++ src/defigraph/Edge.py
@@ -5,12 +5,16 @@ class Edge:
   def __init__(self, pool: Pool):
     self.pool = pool
     self.weight = -log(self.pool.token0_price)
+    self._tuple = [self.pool.token0, self.pool.token1, (self.weight, self.pool)]
 
   def __repr__(self):
-    return f"({self.pool.token0}, {self.pool.token1}, {self.weight})"
+    return f"({[i for i in self._tuple]})"
   
   def __eq__(self, edge):
     return self.pool == edge.pool
   
   def __ne__(self, edge):
     return self.pool != edge.pool
+
+  def __iter__(self):
+    return (i for i in self._tuple)
