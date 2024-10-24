eec36e5f790bbbb08194f7d673ab2c1c1adee785
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2024-10-23 23:04:04 -0500

    ⚡️ minor updates and cleanup

--- src/defigraph/Pool.py
+++ src/defigraph/Pool.py
@@ -4,7 +4,7 @@ from web3 import Web3
 class Pool:
   def __init__(self, pool_address: str, token0: Vertex, token1: Vertex, fee: int, token0_price: float, token1_price: float):
     if token1 == token0: raise ValueError("Token0 should not equal Token1")
-    if not Web3.is_checksum_address(pool_address): raise ValueError("Address is not a valud checksum address")
+    if not Web3.is_checksum_address(pool_address): raise ValueError("Address is not a valid checksum address")
     if type(token0_price) != float and type(token1_price) != float: raise ValueError(f"Token prices should be of type {float}")
     self.address = pool_address
     self.token0 = token0
